f1f249adb267d1624153aa5b4b1ce523e6c01161
noahj <tyreenj@mail.uc.edu>
2025-09-19 13:50:14 -0400

    Commit 1

---
+++ src/ShortLister.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ShortLister {
+
+    public static ArrayList<Object> collect(ArrayList<Object> objects, Filter f) {
+        ArrayList<Object> retVal = new ArrayList<>();
+        for (Object o : objects) {
+            if(f.accept(o)) {
+                retVal.add(o);
+            }
+        }
+        return retVal;
+    }
+
+    public static void main(String[] args)
+    {
+        JFileChooser chooser = new JFileChooser();
+        Scanner inFile;
+        String line;
+        Path target = new File(System.getProperty("user.dir")).toPath();
+        target = target.resolve("src");
+        // set the chooser to the project src directory
+        chooser.setCurrentDirectory(target.toFile());
+
+        try  // Code that might trigger the exception goes here
+        {
+
+            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            {
+                target = chooser.getSelectedFile().toPath();  // this is a File object not a String filename
+
+                inFile = new Scanner(target);
+
+                ArrayList<Object> words = new ArrayList<>();
+                while(inFile.hasNextLine())
+                {
+                    line = inFile.nextLine();
+                    words.add(line);
+                }
+                words = collect(words, new ShortWordFilter());
+                System.out.println(words);
+
+                inFile.close();
+            }
+            else   // User did not pick a file, closed the chooser
+            {
+                System.out.println("Sorry, you must select a file! Termininating!");
+                System.exit(0);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File Not Found Error");
+            e.printStackTrace();
+        }
+        catch (IOException e) // code to handle this exception
+        {
+            System.out.println("IOException Error");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ShortWordFilter.java
@@ -0,0 +1,10 @@
+public class ShortWordFilter implements Filter {
+    @Override
+    public boolean accept(Object x) {
+        if (x instanceof String) {
+            String s = (String) x;
+            return s.length() < 5;
+        }
+        return false;
+    }
+}
